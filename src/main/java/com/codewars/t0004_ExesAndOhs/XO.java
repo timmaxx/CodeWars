f1a2f092305c7d5ee66df79d5f1d6a4d7b61e456
“Maksim <m.n.timchenko@gmail.com>
2023-08-21 15:15:45 +0300

    solve Exes and Ohs

--- src/main/java/com/codewars/t0004_ExesAndOhs/XO.java
+++ src/main/java/com/codewars/t0004_ExesAndOhs/XO.java
@@ -3,8 +3,20 @@ package com.codewars.t0004_ExesAndOhs;
 public class XO {
 
     public static boolean getXO (String str) {
+        int countOfX = 0;
+        int countOfO = 0;
+        String strLowCase = str.toLowerCase();
 
-        // Good Luck!!
-        return false;
+        for (int i = 0; i < strLowCase.length(); i++) {
+            if(strLowCase.charAt(i) == 'x') {
+                countOfX++;
+                continue;
+            }
+            if(strLowCase.charAt(i) == 'o') {
+                countOfO++;
+            }
+        }
+
+        return countOfX == countOfO;
     }
 }
