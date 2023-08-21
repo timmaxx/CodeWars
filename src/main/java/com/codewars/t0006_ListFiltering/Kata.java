87fb31a325b93051b2f9eff2681bad9bb8a2d378
“Maksim <m.n.timchenko@gmail.com>
2023-08-21 17:40:10 +0300

    add List Filtering

---
+++ src/main/java/com/codewars/t0006_ListFiltering/Kata.java
@@ -0,0 +1,22 @@
+package com.codewars.t0006_ListFiltering;
+
+import java.util.List;
+
+// 7 kyu
+//List Filtering
+/*
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+Kata.filterList(List.of(1, 2, "a", "b")) => List.of(1,2)
+Kata.filterList(List.of(1, 2, "a", "b", 0, 15)) => List.of(1,2,0,15)
+Kata.filterList(List.of(1, 2, "a", "b", "aasf", "1", "123", 231)) => List.of(1, 2, 231)
+*/
+
+public class Kata {
+
+    public static List<Object> filterList(final List<Object> list) {
+        // Return the List with the Strings filtered out
+        return null;
+    }
+}
