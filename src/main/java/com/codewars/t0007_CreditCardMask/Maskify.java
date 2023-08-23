829bb2353f5970d4c49a3799bd05df61e0834120
“Maksim <m.n.timchenko@gmail.com>
2023-08-23 23:45:05 +0300

    add Credit Card Mask

---
+++ src/main/java/com/codewars/t0007_CreditCardMask/Maskify.java
@@ -0,0 +1,7 @@
+package com.codewars.t0007_CreditCardMask;
+
+public class Maskify {
+    public static String maskify(String str) {
+        throw new UnsupportedOperationException("Unimplemented");
+    }
+}
