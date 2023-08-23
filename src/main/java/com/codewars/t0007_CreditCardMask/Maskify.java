56c7eb3ffceef46da8a897269a37d0d7c438b351
“Maksim <m.n.timchenko@gmail.com>
2023-08-24 00:05:04 +0300

    solve Credit Card Mask

--- src/main/java/com/codewars/t0007_CreditCardMask/Maskify.java
+++ src/main/java/com/codewars/t0007_CreditCardMask/Maskify.java
@@ -2,6 +2,11 @@ package com.codewars.t0007_CreditCardMask;
 
 public class Maskify {
     public static String maskify(String str) {
-        throw new UnsupportedOperationException("Unimplemented");
+        StringBuilder result = new StringBuilder();
+        result.append("#".repeat(Math.max(0, str.length() - 4)));
+        for (int i = (Math.max(str.length() - 4, 0)); i < str.length(); i++) {
+            result.append(str.charAt(i));
+        }
+        return result.toString();
     }
 }
