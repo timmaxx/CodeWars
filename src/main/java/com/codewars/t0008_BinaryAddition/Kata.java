d3d03249056ea1c96b47ce2b342c59ff45e7289f
“Maksim <m.n.timchenko@gmail.com>
2023-08-24 09:48:25 +0300

    solve Binary Addition

--- src/main/java/com/codewars/t0008_BinaryAddition/Kata.java
+++ src/main/java/com/codewars/t0008_BinaryAddition/Kata.java
@@ -3,7 +3,12 @@ package com.codewars.t0008_BinaryAddition;
 public class Kata{
 
     public static String binaryAddition(int a, int b){
-        //your code here
-        return "";
+        int intSum = a + b;
+        StringBuilder strBinarySum = new StringBuilder();
+        while (intSum != 0) {
+            strBinarySum.insert( 0, intSum % 2 == 0 ? '0': '1');
+            intSum /= 2;
+        }
+        return strBinarySum.toString();
     }
 }
