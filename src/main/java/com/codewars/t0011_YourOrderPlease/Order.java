a66cae7111045d0d76a78231e12bba6b42ee736c
“Maksim <m.n.timchenko@gmail.com>
2023-08-24 19:30:29 +0300

    add Your order, please

---
+++ src/main/java/com/codewars/t0011_YourOrderPlease/Order.java
@@ -0,0 +1,23 @@
+package com.codewars.t0011_YourOrderPlease;
+
+/* Your order, please
+345273191% of 13,41816,080 of 142,031iamstone2 Issues Reported
+ Java 17 VIM EMACS Instructions
+Output
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+*/
+
+public class Order {
+    public static String order(String words) {
+        return "";
+    }
+}
