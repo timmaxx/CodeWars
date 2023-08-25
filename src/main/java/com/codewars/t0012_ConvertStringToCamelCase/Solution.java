abc517cfe1f64ff9a0a83f1364dd314eaa4a4741
“Maksim <m.n.timchenko@gmail.com>
2023-08-25 08:48:58 +0300

    add Convert string to camel case

---
+++ src/main/java/com/codewars/t0012_ConvertStringToCamelCase/Solution.java
@@ -0,0 +1,30 @@
+package com.codewars.t0012_ConvertStringToCamelCase;
+
+/*
+* 6 kyu
+Convert string to camel case
+280051288% of 14,68919,622 of 158,147jhoffner
+Java
+TRAINNEXT KATA
+Details
+Solutions
+Forks (37)
+Discourse (863)
+Collect|
+DESCRIPTION:
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+* */
+
+class Solution{
+
+    static String toCamelCase(String s){
+        return "";
+    }
+}
---
+++ src/test/java/com/codewars/t0012_ConvertStringToCamelCase/SolutionTest.java
@@ -0,0 +1,19 @@
+package com.codewars.t0012_ConvertStringToCamelCase;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void testSomeUnderscoreLowerStart() {
+        String input = "the_Stealth_Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", Solution.toCamelCase(input));
+    }
+    @Test
+    public void testSomeDashLowerStart() {
+        String input = "the-Stealth-Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", Solution.toCamelCase(input));
+    }
+}
