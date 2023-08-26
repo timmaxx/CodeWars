6942796d2eb7d1c7eb0beb4de854725014a951dc
“Maksim <m.n.timchenko@gmail.com>
2023-08-26 10:13:55 +0300

    solve Two Sum

--- src/main/java/com/codewars/t0013_TwoSum/Solution.java
+++ src/main/java/com/codewars/t0013_TwoSum/Solution.java
@@ -1,7 +1,44 @@
 package com.codewars.t0013_TwoSum;
 
+/*
+6 kyu
+Two Sum
+1305
+344
+89% of 5,311
+8,796 of 62,434
+wthit56
+11 Issues Reported
+Java
+17
+VIM
+EMACS
+Instructions
+Output
+Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+Based on: http://oj.leetcode.com/problems/two-sum/
+
+two_sum([1, 2, 3], 4) == {0, 2}
+*/
+
 public class Solution {
-    public static int[] twoSum(int[] numbers, int target) {
-        return null; // Do your magic!
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+
+        return result;
     }
 }
